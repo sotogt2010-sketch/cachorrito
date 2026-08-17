99a0426a75332fb6b5baf0eff87e68933543cc1c
sotogt2010-sketch <sotogt2010@gmail.com>
2026-08-17 04:24:17 -0400

    add splash activity

---
+++ app/src/main/java/com/animales/app/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.animales.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 1200);
+    }
+}
